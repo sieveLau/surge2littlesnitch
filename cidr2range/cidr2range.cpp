d31736cab20b208bd18d8e834e86921c994dbf39
Sieve Lau <sievelau@gmail.com>
2023-08-30 01:02:32 +0800

    完善test case；修复不合法地址在没有dr时直接返回的错误

--- cidr2range/cidr2range.cpp
+++ cidr2range/cidr2range.cpp
@@ -107,172 +107,65 @@ void set_bits(unsigned char *const in, const int full_length, const int dr, cons
 }
 
 string cidr2range(const string &cidr) {
-    /*const int IPV4_LENGTH = 32;
-    const int IPV6_LENGTH = 128;*/
+    const int IPV4_LENGTH = 32;
+    const int IPV6_LENGTH = 128;
     //   unsigned char buf[sizeof(struct in6_addr)];
 
     auto ci_dr = cidr.find_first_of('/');
     int dr;
     string ci, return_val;
     ci = cidr.substr(0, ci_dr);
-    if (ci_dr == std::string::npos) {
-        return_val.append(ci).append("-").append(ci);
-        return return_val;
-    } else {
-        dr = stoi(cidr.substr(ci_dr + 1));
-    }
 
-    // int domain = 4;
     unsigned char in[sizeof(struct in6_addr)](0x0);
     //    string bit_str;
     auto inet_result = inet_pton(AF_INET, ci.c_str(), &in);
 
     if (inet_result > 0) {
-        if (dr > 32) {
+        if (ci_dr == std::string::npos) {
+            return_val.append(ci).append("-").append(ci);
+            return return_val;
+        } else {
+            dr = stoi(cidr.substr(ci_dr + 1));
+        }
+        if (dr > IPV4_LENGTH) {
             std::cerr << "Invalid dr." << endl;
             throw std::out_of_range("Invalid dr for IPv4: " + dr);
-        } else if (dr == 32) {
+        } else if (dr == IPV4_LENGTH) {
             return_val.append(ci).append("-").append(ci);
             return return_val;
         }
-        set_bits(in, 32, dr, 0);
+        set_bits(in, IPV4_LENGTH, dr, 0);
         char ip_buffer[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &in, ip_buffer, INET_ADDRSTRLEN);
         return_val.append(ip_buffer).append("-");
         ip_buffer[0] = '\0';
-        set_bits(in, 32, dr, 1);
+        set_bits(in, IPV4_LENGTH, dr, 1);
         inet_ntop(AF_INET, &in, ip_buffer, INET_ADDRSTRLEN);
         return_val.append(ip_buffer);
         return return_val;
     } else if ((inet_result = inet_pton(AF_INET6, ci.c_str(), &in)) > 0) {
-        if (dr > 128) {
+        if (ci_dr == std::string::npos) {
+            return_val.append(ci).append("-").append(ci);
+            return return_val;
+        } else {
+            dr = stoi(cidr.substr(ci_dr + 1));
+        }
+        if (dr > IPV6_LENGTH) {
             std::cerr << "Invalid dr." << endl;
             throw std::out_of_range("Invalid dr for IPv6: " + dr);
-        } else if (dr == 128) {
+        } else if (dr == IPV6_LENGTH) {
             return_val.append(ci).append("-").append(ci);
             return return_val;
         }
-        set_bits(in, 128, dr, 0);
+        set_bits(in, IPV6_LENGTH, dr, 0);
         char ip_buffer[INET6_ADDRSTRLEN];
         inet_ntop(AF_INET6, &in, ip_buffer, INET6_ADDRSTRLEN);
         return_val.append(ip_buffer).append("-");
         ip_buffer[0] = '\0';
-        set_bits(in, 128, dr, 1);
+        set_bits(in, IPV6_LENGTH, dr, 1);
         inet_ntop(AF_INET6, &in, ip_buffer, INET6_ADDRSTRLEN);
         return_val.append(ip_buffer);
         return return_val;
     }
     throw std::runtime_error("Invalid address.");
-    // if (inet_result > 0) {
-    // std::bitset<IPV4_LENGTH> bit(in);
-    //        bit = reverse_by_group(bit, IPV4_LENGTH / 4);
-    //        cout << bit << endl;
-    //        dr=32;
-    //        bitset<32> netmask_in_binary;
-    //        netmask_in_binary.set();
-    //        if (ci_dr != std::string::npos) dr = stoi(cidr.substr(ci_dr+1));
-    //        int bits_to_move = 32 - dr;
-    //        netmask_in_binary <<= bits_to_move;
-    //        auto begin_addr_in_binary = bit & netmask_in_binary;
-    //        auto end_addr_in_binary = bit | netmask_in_binary.flip();
-    //        auto begin_bit = reverse_by_group(begin_addr_in_binary, 8);
-    //        auto end_bit = reverse_by_group(end_addr_in_binary, 8);
-    //        char end_text[IPV4_LENGTH];
-    //        char begin_text[IPV4_LENGTH];
-    //        in4.s_addr=begin_bit.to_ulong();
-    //        inet_ntop(AF_INET, &in4, begin_text, IPV4_LENGTH);
-    //        in4.s_addr=end_bit.to_ulong();
-    //        inet_ntop(AF_INET, &in4, end_text, IPV4_LENGTH);
-    //        auto* fmt = "%s-%s";
-    //
-    //        char range_buffer[31]{'\0'};
-    //        snprintf(range_buffer,31,fmt,begin_text,end_text);
-    //        cout << range_buffer <<endl;
-    //        return range_buffer;
-    //    } else if ( (inet_result = inet_pton(AF_INET6, ci.c_str(), &in6))>0 ) {
-    //        string bits;
-    //        for(int i = 0; i < 16; ++i){
-    //            std::bitset<8> bit(in6.s6_addr[i]);
-    //            cout<<i<<bit.to_string()<<endl;
-    //        }
-    //
-    //        std::bitset<IPV6_LENGTH> bit(in6.s6_addr);
-    //
-    //        bit = reverse_by_group(bit, 16);
-    //        cout << bit << endl;
-    //        dr=128;
-    //        bitset<128> netmask_in_binary;
-    //        netmask_in_binary.set();
-    //        if (ci_dr != std::string::npos) dr = stoi(cidr.substr(ci_dr+1));
-    //        int bits_to_move = 128 - dr;
-    //        netmask_in_binary <<= bits_to_move;
-    //        auto begin_addr_in_binary = bit & netmask_in_binary;
-    //        auto end_addr_in_binary = bit | netmask_in_binary.flip();
-    //        auto begin_bit = reverse_by_group(begin_addr_in_binary, 16);
-    //        auto end_bit = reverse_by_group(end_addr_in_binary, 16);
-    //        char end_text[IPV6_LENGTH];
-    //        char begin_text[IPV6_LENGTH];
-    ////        in6.s6_addr=begin_bit.to_ulong();
-    //        inet_ntop(AF_INET, &in4, begin_text, IPV4_LENGTH);
-    ////        in4.s_addr=end_bit.to_ulong();
-    //        inet_ntop(AF_INET, &in4, end_text, IPV4_LENGTH);
-    //        auto* fmt = "%s-%s";
-    //
-    //        char range_buffer[31]{'\0'};
-    //        snprintf(range_buffer,31,fmt,begin_text,end_text);
-    //        cout << range_buffer <<endl;
-    //        return range_buffer;
-    //    } else {
-    //        std::cerr << "Not a valid address." << endl;
-    //        return "";
-    //    }
-
-    //
-    //
-    //    if (regex_match(cidr.c_str(), m, ipv4)) {
-    //        auto first_eight_bits = stoi(m[1]);
-    //        auto second_eight_bits = stoi(m[2]);
-    //        auto third_eight_bits = stoi(m[3]);
-    //        auto forth_eight_bits = stoi(m[4]);
-    //
-    //        bitset<32> origin_in_binary
-    //            ((first_eight_bits << 24) + (second_eight_bits << 16) + (third_eight_bits << 8) + forth_eight_bits);
-    //        cout << origin_in_binary << endl;
-    //        bitset<32> netmask_in_binary;
-    //        netmask_in_binary.set();
-    //
-    //        // cout<<netmask_in_binary<<endl;
-    //        // cout<<origin_in_binary<<endl;
-    //        auto begin_addr_in_binary = origin_in_binary & netmask_in_binary;
-    //        auto end_addr_in_binary = origin_in_binary | netmask_in_binary.flip();
-    //
-    //        const bitset<32> one("11111111000000000000000000000000");
-    //        const bitset<32> two("00000000111111110000000000000000");
-    //        const bitset<32> three("00000000000000001111111100000000");
-    //        const bitset<32> four("00000000000000000000000011111111");
-    //
-    //        // cout<<begin_addr_in_binary<<endl;
-    //        // cout<<end_addr_in_binary<<endl;
-    //        snprintf(ipv4_result_range, std::size(ipv4_result_range),
-    //                 ipv4_range_fmt,
-    //                 ((begin_addr_in_binary & one) >> 24).to_ulong(),
-    //                 ((begin_addr_in_binary & two) >> 16).to_ulong(),
-    //                 ((begin_addr_in_binary & three) >> 8).to_ulong(),
-    //                 (begin_addr_in_binary & four).to_ulong(),
-    //                 ((end_addr_in_binary & one) >> 24).to_ulong(),
-    //                 ((end_addr_in_binary & two) >> 16).to_ulong(),
-    //                 ((end_addr_in_binary & three) >> 8).to_ulong(),
-    //                 (end_addr_in_binary & four).to_ulong());
-    //        // cout<<((begin_addr_in_binary & one)>>24).to_ulong()<<endl;
-    //        // cout<<((begin_addr_in_binary & two)>>16).to_ulong()<<endl;
-    //        // cout<<((begin_addr_in_binary & three)>>8).to_ulong()<<endl;
-    //        // cout<<(begin_addr_in_binary & four).to_ulong()<<endl;
-    //
-    //        // cout<<((end_addr_in_binary & one)>>24).to_ulong()<<endl;
-    //        // cout<<((end_addr_in_binary & two)>>16).to_ulong()<<endl;
-    //        // cout<<((end_addr_in_binary & three)>>8).to_ulong()<<endl;
-    //        // cout<<(end_addr_in_binary & four).to_ulong()<<endl;
-    //        cout << ipv4_result_range << endl;
-    //        return ipv4_result_range;
-    //    }
 }
--- cidr2range/test.cpp
+++ cidr2range/test.cpp
@@ -20,9 +20,37 @@ TEST(IPV4, FREE_BIT_ALIGNED) {
 
 TEST(IPV4, NO_FREE_BIT) {
     EXPECT_STREQ(cidr2range("118.89.204.198/32").c_str(), "118.89.204.198-118.89.204.198");
+    EXPECT_STREQ(cidr2range("118.89.204.198").c_str(), "118.89.204.198-118.89.204.198");
     EXPECT_STREQ(cidr2range("118.89.0.198/32").c_str(), "118.89.0.198-118.89.0.198");
     EXPECT_STREQ(cidr2range("118.0.204.198/32").c_str(), "118.0.204.198-118.0.204.198");
     EXPECT_STREQ(cidr2range("118.0.0.198/32").c_str(), "118.0.0.198-118.0.0.198");
+}
+
+TEST(IPV4, INVALID_DR){
+    EXPECT_ANY_THROW(cidr2range("118.89.204.198/33"));
+}
+
+TEST(IPV4, INVALID_ADDR){
+    EXPECT_ANY_THROW(cidr2range("118.89.204:198/31"));
+    EXPECT_ANY_THROW(cidr2range("118.89.204.257"));
+}
+
+TEST(IPV6, FREE_BIT_NOT_ALIGNED) {
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/127").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:96b6-2402:4e00:1200:ed00:eef1:9089:6dac:96b7");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/126").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:96b4-2402:4e00:1200:ed00:eef1:9089:6dac:96b7");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/125").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:96b0-2402:4e00:1200:ed00:eef1:9089:6dac:96b7");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/124").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:96b0-2402:4e00:1200:ed00:eef1:9089:6dac:96bf");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/123").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:96a0-2402:4e00:1200:ed00:eef1:9089:6dac:96bf");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/122").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:9680-2402:4e00:1200:ed00:eef1:9089:6dac:96bf");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/121").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:9680-2402:4e00:1200:ed00:eef1:9089:6dac:96ff");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/120").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:9600-2402:4e00:1200:ed00:eef1:9089:6dac:96ff");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/119").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:9600-2402:4e00:1200:ed00:eef1:9089:6dac:97ff");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/118").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:9400-2402:4e00:1200:ed00:eef1:9089:6dac:97ff");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/117").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:9000-2402:4e00:1200:ed00:eef1:9089:6dac:97ff");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/116").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:9000-2402:4e00:1200:ed00:eef1:9089:6dac:9fff");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/115").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:8000-2402:4e00:1200:ed00:eef1:9089:6dac:9fff");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/114").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:8000-2402:4e00:1200:ed00:eef1:9089:6dac:bfff");
+        EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/113").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:8000-2402:4e00:1200:ed00:eef1:9089:6dac:ffff");
 }
 
 TEST(IPV6, FREE_BIT_ALIGNED) {
@@ -44,11 +72,22 @@ TEST(IPV6, FREE_BIT_ALIGNED) {
                  "::-ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
 }
 
-// int main(int argc, char **argv) {
-//
-//   // printf("%s\n",cidr2range(argv[1]).c_str());
-//   assert(cidr2range("118.89.204.198/23") == "118.89.204.0-118.89.205.255");
-//   assert(cidr2range("118.89.204.198/32") == "118.89.204.198-118.89.204.198");
-//   assert(cidr2range("2402:4e00:1200:ed00:0:9089:6dac:96b6/112") ==
-//   "2402:4e00:1200:ed00:0:9089:6dac:0-2402:4e00:1200:ed00:0:9089:6dac:ffff"); return 0;
-// }
+TEST(IPV6, NO_FREE_BIT) {
+    EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6/128").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:96b6-2402:4e00:1200:ed00:eef1:9089:6dac:96b6");
+    EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:96b6").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:96b6-2402:4e00:1200:ed00:eef1:9089:6dac:96b6");
+    EXPECT_STREQ(cidr2range("2402:0:1200:ed00:eef1:9089:6dac:96b6/128").c_str(), "2402:0:1200:ed00:eef1:9089:6dac:96b6-2402:0:1200:ed00:eef1:9089:6dac:96b6");
+    EXPECT_STREQ(cidr2range("2402:4e00:0:ed00:eef1:9089:6dac:96b6/128").c_str(), "2402:4e00:0:ed00:eef1:9089:6dac:96b6-2402:4e00:0:ed00:eef1:9089:6dac:96b6");
+    EXPECT_STREQ(cidr2range("2402:4e00:1200:0:eef1:9089:6dac:96b6/128").c_str(), "2402:4e00:1200:0:eef1:9089:6dac:96b6-2402:4e00:1200:0:eef1:9089:6dac:96b6");
+    EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:0:9089:6dac:96b6/128").c_str(), "2402:4e00:1200:ed00:0:9089:6dac:96b6-2402:4e00:1200:ed00:0:9089:6dac:96b6");
+    EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:0:6dac:96b6/128").c_str(), "2402:4e00:1200:ed00:eef1:0:6dac:96b6-2402:4e00:1200:ed00:eef1:0:6dac:96b6");
+    EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:0:96b6/128").c_str(), "2402:4e00:1200:ed00:eef1:9089:0:96b6-2402:4e00:1200:ed00:eef1:9089:0:96b6");
+    EXPECT_STREQ(cidr2range("2402:4e00:1200:ed00:eef1:9089:6dac:0/128").c_str(), "2402:4e00:1200:ed00:eef1:9089:6dac:0-2402:4e00:1200:ed00:eef1:9089:6dac:0");
+}
+
+TEST(IPV6, INVALID_DR){
+    EXPECT_ANY_THROW(cidr2range("2402:4e00:1200:ed00:0:9089:6dac:96b6/129"));
+}
+
+TEST(IPV6, INVALID_ADDR){
+    EXPECT_ANY_THROW(cidr2range("2402:4e00:1200:ed00:0:9089:6dac:gggg"));
+}
